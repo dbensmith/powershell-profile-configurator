26904435b87cf322b81de8d476d76f5ece13619e
Benjamin Smith <10767946+dbensmith@users.noreply.github.com>
2023-05-11 11:20:26 -0600

    Update Microsoft.PowerShell_profile.ps1
    Update oh-my-posh invocation to avoid mistaken A/V detections

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -39,4 +39,5 @@ foreach ($Module in $RequiredModules) {
 
 # Set oh-my-posh theme and enable use of posh-git
 $env:POSH_GIT_ENABLED = $true
-oh-my-posh --init --shell pwsh --config $env:POSH_THEMES_PATH\$Theme.omp.json | Invoke-Expression
+& ([ScriptBlock]::Create((oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\$Theme.omp.json" --print) -join "`n")) # Avoids misinformed A/V detections
+#oh-my-posh init shell pwsh --config $env:POSH_THEMES_PATH\$Theme.omp.json | Invoke-Expression
