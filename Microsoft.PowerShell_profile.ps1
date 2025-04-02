85c9a090bcffdeb483683786a45925d86fa7878f
Benjamin Smith <10767946+dbensmith@users.noreply.github.com>
2025-04-02 14:16:31 -0600

    Update comment to clarify purpose of oh-my-posh initialization command

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -42,5 +42,4 @@ foreach ($Module in $RequiredModules) {
 # Set oh-my-posh theme and enable use of posh-git
 $Theme = "slimfat"
 $env:POSH_GIT_ENABLED = $true
-& ([ScriptBlock]::Create((oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\$Theme.omp.json" --print) -join "`n")) # Avoids misinformed A/V detections
-#oh-my-posh init shell pwsh --config $env:POSH_THEMES_PATH\$Theme.omp.json | Invoke-Expression
+& ([ScriptBlock]::Create((oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\$Theme.omp.json" --print) -join "`n")) # Avoids false positives by A/V and EDR tools
